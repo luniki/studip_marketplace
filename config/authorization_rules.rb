7dcfd79baa04f1139b3109fdbde782e349a3442b
Marcus Lunzenauer <mlunzena@uos.de>
2010-04-02 18:41:46 +0200

    simplify authz rules

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -1,21 +1,29 @@
 authorization do
   role :guest do
-    has_permission_on :plugins, :to => [:index, :show]
-    has_permission_on :releases, :to => [:index, :show]
+    has_permission_on :plugins, :to => :read
+    has_permission_on :releases, :to => :read
   end
 
   role :author do
     includes :guest
 
-    has_permission_on :plugins, :to => [:new, :create]
+    has_permission_on :plugins, :to => :create
 
     has_permission_on :plugins, :to => [:update, :delete] do
       if_attribute :user => is { user }
     end
 
-    has_permission_on :releases, :to => [:new, :create] do
+    has_permission_on :releases, :to => :create do
       if_permitted_to :update, :plugin
     end
-  end
 
+  end
+end
+
+privileges do
+  privilege :manage, :includes => [:create, :read, :update, :delete]
+  privilege :read, :includes => [:index, :show]
+  privilege :create, :includes => :new
+  privilege :update, :includes => :edit
+  privilege :delete, :includes => :destroy
 end
